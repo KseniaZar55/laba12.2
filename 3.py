f582bfe791846bdc9b86c2a6aa837a03fde46456
KseniaZar55 <160110507+KseniaZar55@users.noreply.github.com>
2024-05-23 01:44:28 +0300

    laba12

---
+++ 3.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+root.title("Кафе Мороженка")
+root.geometry("400x300")
+
+label = Label(text="Доступные вкусы мороженого: ")
+gjgfj = Label(text="1) Ваниль")
+vv = Label(text="2) Шоколад")
+jj = Label(text="3) Клубника")
+label.pack()
+gjgfj.pack()
+vv.pack()
+jj.pack()
+root.mainloop()
